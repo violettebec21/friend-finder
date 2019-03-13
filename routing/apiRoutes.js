20ad6b83e1584a0bcc7e0d312f75d13576b02178
violettebec <bechoerner@gmail.com>
2019-03-12 23:50:33 -0500

    make it better

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -36,7 +36,7 @@ module.exports = function (app) {
     });
 
     // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
+    // Below handles when a user submits a form and thus submits data to the server.
     // In each of the below cases, when a user submits form data (a JSON object)
     // ...the JSON is pushed to the appropriate JavaScript array
     // (ex. User fills out survey... this data is then sent to the server...
@@ -45,30 +45,68 @@ module.exports = function (app) {
 
     //    * A POST routes `/api/friends`. 
     //    This will be used to handle incoming survey results. 
-    //      This route will also be used to handle the compatibility logic.
 
     app.post("/app/friends", function (req, res) {
 
+        //      This route will also be used to handle the compatibility logic.
+
+        // ===============================================================================
+        // COMPATIBILITY LOGIC
+        //     Determine the user's most compatible friend using the following as a guide:
+
+        //    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+        //    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+        //      * Example:
+        //        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+        //        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+        //        * Total Difference: **2 + 1 + 2 =** **_5_**
+        //    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+        //    * The closest match will be the user with the least amount of difference.
         // Our "server" will respond to requests and let users know who they are compatible with
-        // Sends out the "true" value for matches
-        // req.body is available since we're using the body parsing middleware
-        //body represents our form data here 
 
+        // create an object to hold "bestFriend"
+        var bestFriend = {
+            name: "",
+            photo: "",
+            scoreDifference: 0,
+        };
 
-        // if (friendData.length < 5) {
-        //     tableData.push(req.body);
-        //     res.json(true);
-        //     //should match compatibility of each response compared to other survery completions
-        //   }
-        //   else {
-        //     waitListData.push(req.body);
-        //     res.json(false);
+        // parse the result of the user submit POST
+        var userData = req.body;
+        var userName = userData.name;
+        var userPhoto = userData.photo;
+        var userScore = userData.score;
 
-        //   }
+        // create a variable to hold the score difference between users
+        var scoreDifference = 0;
+
+        // Loop through the friend array in friends.js
+        for (var i = 0; i < friendData.length; i++) {
+            console.log(friendData[i].name);
+            scoreDifference = 0;
+
+            // Loop through scores for each friend & calcualte difference in scores
+            for (var j = 0; j < friendData[i].scores[j]; j++) {
+                scoreDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendData[i].scores[j]));
+
+                // If the sum of differences is less then the differences of the current "best match"
+                // set the bestFriend match for the user 
+                if (scoreDifference <= bestFriend.friendDifference) {
+                    bestFriend.name = friendData[i].name;
+                    bestFriend.photo = friendData[i].photo;
+                    bestFriend.friendDifference = scoreDifference;
+                }
+            }
+        }
+
+        // push the userData use for HTML
+        friendData.push(userData);
+        res.json(bestFriend);
     });
 
     // ---------------------------------------------------------------------------
     // Clear out the data working with the functionality.
+    //using from in-class example for clear
 
     app.post("/api/clear", function (req, res) {
         // Empty out the arrays of data
